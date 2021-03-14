6c219e85ec7c689913aff004cb8dcb72da55b15d
SMU <smu31081199@gmail.com>
2021-03-14 22:50:09 +0530

    added follower relationship to display followers

--- app/Models/Follower.php
+++ app/Models/Follower.php
@@ -15,8 +15,15 @@ class Follower extends Model
         'follower_id'
     ];
 
+    // Relation between user_id and users
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    // Relation between follower_id and users
+    public function follower()
+    {
+        return $this->belongsTo(User::class, 'follower_id');
     }
 }
