57535c6626530c6b8b9f2e13d026520dbba69e83
SMU <smu31081199@gmail.com>
2021-03-15 22:01:38 +0530

    updated add_image_to_blogs_table migration file

--- database/migrations/2021_03_15_214547_add_image_to_blogs_table.php
+++ database/migrations/2021_03_15_214547_add_image_to_blogs_table.php
@@ -14,7 +14,7 @@ class AddImageToBlogsTable extends Migration
     public function up()
     {
         Schema::table('blogs', function (Blueprint $table) {
-            $table->string('image')->after('body');
+            $table->string('image')->after('body')->nullable();
         });
     }
 
