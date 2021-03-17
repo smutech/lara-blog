c5aea1a627a87a763e4bc13a159fb28ca1990296
SMU <smu31081199@gmail.com>
2021-03-17 15:55:52 +0530

    updated BlogFactory

--- database/factories/BlogFactory.php
+++ database/factories/BlogFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use App\Models\Blog;
+use App\Models\User;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -22,12 +24,22 @@ class BlogFactory extends Factory
      */
     public function definition()
     {
+        $all_users = User::select('id')->get();
+        $users = [];
+
+        foreach ($all_users as $key => $user) {
+            array_push($users, $user->id);
+        }
+
+        $user_id = Arr::random($users);
+
         $title = $this->faker->sentence(10);
         
         return [
             'slug' => Str::slug($title),
             'title' => $title,
-            'body' => $this->faker->paragraph(75)
+            'body' => $this->faker->paragraph(75),
+            'user_id' => $user_id,
         ];
     }
 }
