814188c8d075f1b37731a28eb25102968d7763cb
SMU <smu31081199@gmail.com>
2021-03-17 16:01:27 +0530

    updated FollowerFactory

--- database/factories/FollowerFactory.php
+++ database/factories/FollowerFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use App\Models\Follower;
+use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class FollowerFactory extends Factory
@@ -22,9 +23,24 @@ class FollowerFactory extends Factory
      */
     public function definition()
     {
-        return [
-            'follower_id' => $follower_id,
-            'user_id' => $user->id,
-        ];
+        $all_users = User::select('id')->get();
+        $users = [];
+
+        foreach ($all_users as $key => $user) {
+            array_push($users, $user->id);
+        }
+
+        $follower_id = Arr::random($users);
+        $user_id = Arr::random($users);
+
+        $follower_exists = Follower::where(['follower_id' => $follower_id, 'user_id' => $user_id])->first();
+
+        if (($follower_id != $user_id) && empty($follower_exists)) {
+            return [
+                'follower_id' => $follower_id,
+                'user_id' => $user_id,
+            ];
+        }
+        else { die; }
     }
 }
