1ccd8638dd8cec79edc17db8ce8f88fc1ffd76a7
SMU <smu31081199@gmail.com>
2021-03-17 16:03:34 +0530

    updated DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            // UserSeeder::class,
-            // BlogSeeder::class,
+            UserSeeder::class,
+            BlogSeeder::class,
             FollowerSeeder::class,
         ]);
     }
